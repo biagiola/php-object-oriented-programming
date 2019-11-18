6abaf7ea2bc2bada49afecc9ad0634099d23654c
David <41311088+biagiola@users.noreply.github.com>
2019-11-18 10:32:36 -0300

    Update site.php

--- 03-Constructors/site.php
+++ 03-Constructors/site.php
@@ -1,31 +1,33 @@
+<?php  
+
+	class Book { 
+		var $title;
+		public $author;
+		var $pages;
+
+		/* Constructor function 	*/
+		function __constructor($aTitle, $aAuthor,$aPages){
+			$this->title = $aTitle;
+			$this->author = $aAuthor;
+			$this->pages = $aPages;
+		}		
+	}
+
+	$book1 = new Book("Harry Potter", "JK Rowling", 400);  
+	$book2 = new Book("Lord Of the Rings", "Tolkien", 700);  
+
+	echo $book1->title;
+	echo $book1->author;
+
+
+?>
 <!DOCTYPE html>
+<html>
 <head>
 	<meta charset="utf-8">
 	<title>My web site<title>
 </head>
 <body>
 
-	<?php  
-		
-		class Book { 
-			var $title;
-			var $author;
-			var $pages;
-			
-			/* Constructor function 	*/
-			function __constructor($aTitle, $aAuthor,$aPages){
-				$this->title = $aTitle;
-				$this->author = $aAuthor;
-				$this->pages = $aPages;
-			}		
-		}
-		
-		$book1 = new Book("Harry Potter", "JK Rowling", 400);  
-		$book2 = new Book("Lord Of the Rings", "Tolkien", 700);  
-
-		echo $book1->title;
-		echo $book2->title;
-		
-	?>
-
 </body>
+</html>
