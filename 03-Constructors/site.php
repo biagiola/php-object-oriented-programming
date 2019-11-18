e87aca61a7a8db32d6329bbc306a354a0ecf00ee
David <41311088+biagiola@users.noreply.github.com>
2019-11-18 10:47:35 -0300

    Update site.php

--- 03-Constructors/site.php
+++ 03-Constructors/site.php
@@ -1,16 +1,20 @@
 <?php  
 
 	class Book { 
-		var $title;
+		public $title;
 		public $author;
-		var $pages;
+		public $pages;
 
-		/* Constructor function 	*/
+		// Constructor function 	
 		function __constructor($aTitle, $aAuthor,$aPages){
 			$this->title = $aTitle;
 			$this->author = $aAuthor;
 			$this->pages = $aPages;
-		}		
+		}
+		
+		public function bookAddedMessage() {
+			return " $this->title book added. Written by $this->author ";
+		}
 	}
 
 	$book1 = new Book("Harry Potter", "JK Rowling", 400);  
@@ -18,6 +22,12 @@
 
 	echo $book1->title;
 	echo $book1->author;
+	echo $book1->bookAddedMessage();
+
+	$book1->pages = 455; // we can change values because the class's properties are public.
+	
+	print_r(get_class_var("Book")) // give us all the properties of the class; print_r because it is an array
+	print_r(get_class_methods("Book")) // give us all the methos of the class.
 
 
 ?>
